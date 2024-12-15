ed757de423425fbb07cceedc16a23f8f70acd711
Tushar-Nagdive-Dev <codewithtushar840@gmail.com>
2024-12-15 14:01:38 +0530

    mo

--- src/main/java/com/inn/pcda/configs/Executors.java
+++ src/main/java/com/inn/pcda/configs/Executors.java
@@ -1,12 +1,12 @@
 package com.inn.pcda.configs;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+/* import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; */
 
 public class Executors {
      public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+       /*  BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         String rawPassword = "admin";
         String hashedPassword = encoder.encode(rawPassword);
-        System.out.println("Hashed Password: " + hashedPassword);
+        System.out.println("Hashed Password: " + hashedPassword); */
      }
 }
